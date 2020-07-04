cfc34bbc4451a2b23f3fa36aa11e20d11948d44d
gfike19 <gfike19@gmail.com>
2020-07-04 13:25:37 -0500

    started on unit 4 assignmnet 5

--- unit4/assign5.php
+++ unit4/assign5.php
@@ -3,24 +3,30 @@
 $partNumbers = array(1065, 1095, 1075, 1055, 1056, 1090, 1098, 1088, 1097, 1078);
 $part1 = $_POST["part1"];
 $part2 = $_POST["part2"];
+sort($partNumbers);
 
-function sequentialSearch($arr, $val)
+function binarySearch($arr, $val, $low, $high, $middle, $pos)
 {
-    $flag = 0;
-    for ($i = 0; $i < sizeof($arr); $i++) {
-        if ($val == $arr[$i]) {
-            echo "The part number " . $val . " was found at index " . $i;
-            echo "<br>" . $val . " is in stock<br>";
-            $flag += $val;
-            break;
+    if ($val == $arr[$middle]) {
+        echo "The part number was found at index " . $middle . "<br>";
+        echo $arr[$middle] . " is in stock";
+    } else {
+
+        if ($val < $arr[$middle]) {
+            $pos = $middle - 1;
         }
-        if ($i + 1 == sizeof($arr) and $val != $arr[$i]) {
-            echo $val . " is not in stock<br>";
-            $flag -= 1;
+
+        if ($val > $arr[$middle]) {
+            $pos = $middle + 1;
+        }
+
+        while ($pos != $low and $pos != $high) {
+            
         }
     }
-    return $flag;
 }
 
-sequentialSearch($partNumbers, $part1);
-sequentialSearch($partNumbers, $part2);
+$high = sizeof($arr);
+$low = 0;
+$pos = null;
+$middle = $low + intdiv(($high - 1), 2);
