2214891fed6e122fc1a223bf010a9e6587797de5
gfike19 <gfike19@gmail.com>
2020-07-04 17:10:27 -0500

    code for assignment 5 still not working, going to email professor for advice

--- unit4/assign5.php
+++ unit4/assign5.php
@@ -1,32 +1,56 @@
 <?php
 
+function binarySearch($arr, $val, $low, $high, $middle, $len)
+{   
+    // below line is for testing purposes
+    echo "<p>Low is currently: " . $low . "<br>High is currently: " . $high . "<br>Middle is currently: " . $middle . "</p>";
+
+    if ($val == $arr[$middle]) {
+        echo "The part number was found at index " . $middle . "<br>";
+        echo $val . " is in stock";
+    return;
+    } else {
+        if ($high > $len + 1 and $val != $arr[$middle]) {
+            echo $val . " is not in stock<br>";
+            return;
+        }
+
+        if ($low < 0 and $val != $arr[$middle]) {
+            echo $val . " is not in stock<br>";
+            return;
+        }
+
+        if ($val == $arr[$middle]) {
+            echo "The part number was found at index " . $middle . "<br>";
+            echo $val . " is in stock";
+        return;
+        }
+        // while ($high != $middle and $low != $middle) {
+
+            if ($val > $arr[$middle]) {
+                $low = $middle + 0;
+                $middle = findMiddle($low, $high);
+                binarySearch($arr, $val, $low, $high, $middle, $len);
+            }
+            if ($val < $arr[$middle]) {
+                $high = $middle + 0;
+                $middle = findMiddle($low, $high);
+                binarySearch($arr, $val, $low, $high, $middle, $len);
+            }
+        //}
+    }
+}
+
+function findMiddle($low, $high)
+{
+    return $low + intdiv(($high - 1), 2);
+}
+
 $partNumbers = array(1065, 1095, 1075, 1055, 1056, 1090, 1098, 1088, 1097, 1078);
 $part1 = $_POST["part1"];
 $part2 = $_POST["part2"];
 sort($partNumbers);
-
-function binarySearch($arr, $val, $low, $high, $middle, $pos)
-{
-    if ($val == $arr[$middle]) {
-        echo "The part number was found at index " . $middle . "<br>";
-        echo $arr[$middle] . " is in stock";
-    } else {
-
-        if ($val < $arr[$middle]) {
-            $pos = $middle - 1;
-        }
-
-        if ($val > $arr[$middle]) {
-            $pos = $middle + 1;
-        }
-
-        while ($pos != $low and $pos != $high) {
-            
-        }
-    }
-}
-
-$high = sizeof($arr);
-$low = 0;
-$pos = null;
-$middle = $low + intdiv(($high - 1), 2);
+$len = sizeof($partNumbers) - 1;
+$middle = findMiddle(0, $len);
+binarySearch($partNumbers, $part1, 0, $len, $middle, $len);
+binarySearch($partNumbers, $part2, 0, $len, $middle, $len);
